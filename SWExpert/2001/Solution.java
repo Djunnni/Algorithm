3ade118b958cf42a0dc1310d08c02f741db91c54
Dongjoon Lee <djunnni@gmail.com>
2022-08-03 17:48:59 +0900

    update: 2001 / 2중 for문에서 dp 형태로 변경

--- SWExpert/2001/Solution.java
+++ SWExpert/2001/Solution.java
@@ -1,94 +1,47 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Arrays;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+/**
+ * D2_2001_파리퇴치
+ * 배열 내에서 해당하는 크기만큼을 잡아 가장 큰 수를 리턴합니다.
+ * author djunnni
  */
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-        //System.setIn(new FileInputStream("res/input.txt"));
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-
-            /////////////////////////////////////////////////////////////////////////////////////////////
 			int N = sc.nextInt();
-            int M = sc.nextInt();
-
-            int[][] matrix = new int[N][N];
-
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    matrix[i][j] = sc.nextInt();
-                }
-            }
-
-            int maxKill = 0;
-            for(int x = N - M; x >= 0; x--) {
-                for(int y = N - M; y >= 0; y--) {
-                    int kill = 0;
-                    // matrix[x][y]를 시작으로 스퀘어의 값을 모두 더하도록 짜자
-                    for(int i = M - 1; i >= 0; i--) {
-                        for(int j = M - 1; j >= 0; j--) {
-                            kill += matrix[x + i][y + j];
-                        }
-                    }
-                    if(kill > maxKill) {
-                        maxKill = kill;
-                    }
-                }
-            }
-
-            System.out.println("#" + test_case + " " + maxKill);
-            /////////////////////////////////////////////////////////////////////////////////////////////
-
-        }
-    }
+			int M = sc.nextInt();
+			
+			int matrix[][] = new int[N + 1][N + 1];
+			int accumulate[][] = new int[N + 1][N + 1];
+			
+			// i 는 열, j는 행
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= N; j++) {
+					matrix[i][j] = sc.nextInt();
+					accumulate[i][j] = matrix[i][j] + accumulate[i][j - 1];
+				}
+			}
+			
+			int MAX = Integer.MIN_VALUE;
+			for(int i = 1; i <= N - M + 1; i++) {
+				for(int j = 1; j <= N - M + 1; j++) {
+					int sum = 0;
+					for(int k = 0; k < M; k++) {
+						sum += accumulate[i + k][j + M - 1] - accumulate[i + k][j - 1];
+					}
+					if(sum > MAX) {
+						MAX = sum;
+					}
+				}
+			}
+			
+			System.out.println("#" + test_case + " " + MAX);
+			
+		}
+	}
 }
