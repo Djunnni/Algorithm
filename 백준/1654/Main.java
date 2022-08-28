af264c45335d323996c8777be1bba52a24c5dc31
djunnni <djunnni@gmail.com>
2022-08-28 13:47:40 +0900

    add 1654

--- 백준/1654/Main.java
+++ 백준/1654/Main.java
@@ -1,39 +1,63 @@
-package solved_ac;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/**
+ * BJ_1654_랜선자르기
+ * author djunnni
+ */
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int K = sc.nextInt();
-        int N = sc.nextInt();
+    public static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int arr[] = new int[K];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        long max = 0;
+        // 이미 가지고 있는 랜선의 개수 1 <= k <= 10,000
+        int K = Integer.parseInt(st.nextToken());
+        // 필요한 랜선의 개수 1 <= n <= 1,000,000
+        N = Integer.parseInt(st.nextToken());
+
+        // 가지고 있는 파이프들의 길이 배열
+        int pipe[] = new int[K];
+
+        // 그중에서 최대 길이의 파이프를 찾는다.
+        int max = Integer.MIN_VALUE;
+
         for(int i = 0; i < K; i++) {
-            arr[i] = sc.nextInt();
-            if(arr[i] > max) {
-                max = arr[i];
+            pipe[i] = Integer.parseInt(br.readLine());
+            if(max < pipe[i]) { // 최대 업데이트
+                max = pipe[i];
             }
         }
-        long min = 1;
-        while(min < max) {
-            long mid = (min + max) / 2;
-            long count = 0;
-            for(int i = 0; i < K; i++) {
-                count += (arr[i] / mid);
+
+        // 가지고 있는 파이프 확인하기
+//        System.out.println(Arrays.toString(pipe));
+//        System.out.println("====================");
+
+        System.out.println(findLan(pipe, max));
+    }
+    public static long findLan(int[] pipe, int end) {
+        long low = 1;
+        long high = end;
+
+        while(low <= high) {
+            int count = 0;
+            long mid = (low + high) / 2;
+
+            for(int i = 0; i < pipe.length; i++) {
+                count += pipe[i] / mid;
             }
-            if(count <= N) {
-                max = mid - 1;
+
+            if(count >= N) {
+                low = mid + 1;
             } else {
-                min = mid;
-            }
-            if(min == mid) {
-                break;
+                high = mid - 1;
             }
         }
-        System.out.println(max + 1);
+
+        return high;
     }
 }
