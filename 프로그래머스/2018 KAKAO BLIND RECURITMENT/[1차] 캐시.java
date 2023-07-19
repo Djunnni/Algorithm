b3cd82fc7196fe02d06409ac786b06342cd53cda
Dongjoon Lee <djunnni@gmail.com>
2023-07-19 23:17:27 +0900

    [프로그래머스] 캐시

--- 프로그래머스/2018 KAKAO BLIND RECURITMENT/[1차] 캐시.java
+++ 프로그래머스/2018 KAKAO BLIND RECURITMENT/[1차] 캐시.java
@@ -8,20 +8,19 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        
+
         // 캐시를 구성한다.
         // 캐시는 나온 순서를 기억해야한다. cache miss가 발생하면 제일 나중에 발견한 아이템을 지운다
         Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        
+
         for(String c: cities) {
             String city = c.toLowerCase(); // 영문자 대소문자 구분을 없애기
-            if(set.contains(city)) { // 가지고 있는 도시라면
+            if(map.containsKey(city)) { // 가지고 있는 도시라면
                 answer += 1;
                 map.put(city, answer); // 최근 초로 반영하기
             } else { // 없는 도시라면
                 answer += 5;
-                if(set.size() == cacheSize) { // 캐시 사이즈와 같아진다면
+                if(map.size() == cacheSize) { // 캐시 사이즈와 같아진다면
                     String key = null;
                     Integer value = Integer.MAX_VALUE;
                     for(Map.Entry<String, Integer> m : map.entrySet()) {
@@ -31,18 +30,13 @@ class Solution {
                         }
                     }
                     map.remove(key);
-                    set.remove(key);
                 }
                 // 새로 반영하기
                 if(cacheSize > 0) {
-                    set.add(city);
                     map.put(city, answer); // 최근 접근한 초를 저장하기
                 }
             }
         }
-        
-        
-        
         return answer;
     }
 }
