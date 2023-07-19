a78cdab33c6ac8e15d48345cc1f7db8ab61f8ff9
Dongjoon Lee <djunnni@gmail.com>
2023-07-19 23:27:16 +0900

    [프로그래머스] 캐시 List 교체

--- 프로그래머스/2018 KAKAO BLIND RECURITMENT/[1차] 캐시.java
+++ 프로그래머스/2018 KAKAO BLIND RECURITMENT/[1차] 캐시.java
@@ -11,32 +11,30 @@ class Solution {
 
         // 캐시를 구성한다.
         // 캐시는 나온 순서를 기억해야한다. cache miss가 발생하면 제일 나중에 발견한 아이템을 지운다
-        Map<String, Integer> map = new HashMap<>();
-
+        List<String> map = new LinkedList<>();
+        
         for(String c: cities) {
             String city = c.toLowerCase(); // 영문자 대소문자 구분을 없애기
-            if(map.containsKey(city)) { // 가지고 있는 도시라면
+            int index = map.indexOf(city);
+            
+            if(index >= 0) { // 가지고 있는 도시라면
                 answer += 1;
-                map.put(city, answer); // 최근 초로 반영하기
+                map.remove(index);
+                map.add(city);
             } else { // 없는 도시라면
                 answer += 5;
-                if(map.size() == cacheSize) { // 캐시 사이즈와 같아진다면
-                    String key = null;
-                    Integer value = Integer.MAX_VALUE;
-                    for(Map.Entry<String, Integer> m : map.entrySet()) {
-                        if(value > m.getValue()) {
-                            key = m.getKey();
-                            value = m.getValue();
-                        }
-                    }
-                    map.remove(key);
+                if(map.size() == cacheSize && cacheSize > 0) { // 캐시 사이즈와 같아진다면
+                    map.remove(0);
                 }
                 // 새로 반영하기
                 if(cacheSize > 0) {
-                    map.put(city, answer); // 최근 접근한 초를 저장하기
+                    map.add(city); // 최근 접근한 초를 저장하기
                 }
             }
         }
+
+
+
         return answer;
     }
 }
